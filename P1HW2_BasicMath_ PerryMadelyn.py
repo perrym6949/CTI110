cbda5dd40f54272d7d18b33c51e0a7d064f44739
Madelyn Perry <78816280+perrym6949@users.noreply.github.com>
2021-05-03 11:27:09 -0400

    Add files via upload

---
+++ P1HW2_BasicMath_ PerryMadelyn.py
@@ -0,0 +1,31 @@
+# Using math with IDLE
+# 2/23/2021
+# CTI-110 P1HW2 - Basic Math
+# Madelyn Perry
+#
+first_num = 14
+second_num = 12
+print('Please Enter first number:', first_num)
+print('Please Enter second number:', second_num)
+
+print('Sum of both numbers:', first_num + second_num)
+print('Result of Multiplying the two numbers:', first_num * second_num)
+
+# START
+# IF  User enters First number <-- 14
+# THEN first_num <-- 14
+# IF User enters Second number <-- 12
+# THEN second_num <-- 12
+# ENDIF
+# PRINT text 'Please Enter first number:', OUTPUT value of first_num
+# PRINT text 'Please Enter second number:', OUTPUT value of second_num
+# IF first_num + second_num = sum of both numbers
+# THEN CALCULATE 14 + 12 = 26
+# ENDIF
+# PRINT text 'Sum of both numbers:', OUTPUT value of first_num + second_num
+# IF first_num * second_num = result of of both numbers
+# THEN CALCULATE 14 * 12 = 168
+# ENDIF
+# PRINT text 'Result of Multiplying the two numbers:', OUTPUT value of first_num * second_num
+# END
+#
